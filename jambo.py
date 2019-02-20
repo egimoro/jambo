fd2949a85bb3d1548b54832558e5971701c6dd02
egimoro <gimoremma@gmail.com>
2019-02-20 22:45:52 +0100

    jua

--- jambo.py
+++ jambo.py
@@ -12,7 +12,7 @@ from flask_mail import Mail,Message
 
 basedir=os.path.abspath(os.path.dirname(__file__))
 app=Flask(__name__)
-app.config['SECRET_KEY']='Swabblegabblejuma@'
+app.config['SECRET_KEY']='Hardjua@'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
 app.config['MAIL_SERVER']='smtp.googleemail.com'
